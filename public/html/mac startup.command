a53ccef38ba8b7131fade3314c7479707b000e50
Wei Junyu <wei_junyu@tech.gov.sg>
2018-02-09 10:55:42 +0800

    Updated webpack production build config and readme.

--- public/html/mac startup.command
+++ public/html/mac startup.command
@@ -1,3 +1,5 @@
 cd "$(dirname "$0")"
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --user-data-dir="/tmp/chrome_dev_session" --disable-web-security --ignorcertificate-errors index.html
-
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome \
+    --user-data-dir="/tmp/chrome_dev_session" \
+    --disable-web-security --ignorcertificate-errors \
+    index.html
--- webpack.config.js
+++ webpack.config.js
@@ -4,41 +4,47 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-    entry: {
-        app: './src/app.js'
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'app.bundle.js'
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        new webpack.NoEmitOnErrorsPlugin(),
-        new webpack.optimize.UglifyJsPlugin(),
-        new ExtractTextPlugin('stylesheets/[name].css'),
-        new OptimizeCssAssetsPlugin(),
-        new HtmlWebpackPlugin({
-            template: 'index.ejs'
-        })
-    ],
-    module: {
-        loaders: [
-            {
-                test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
-                loader: 'url-loader?limit=100000'
-            },
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract(
-                    {
-                        fallback: 'style-loader',
-                        use: 'css-loader'
-                    }
-                )
-            }
-        ]
+module.exports = (env = {}) => { // set env as empty object if unset from cli
+    let config = {
+        entry: {
+            app: './src/app.js'
+        },
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'app.bundle.js'
+        },
+        devServer: {
+            contentBase: path.resolve(__dirname, 'dist')
+        },
+        plugins: [
+            new webpack.NoEmitOnErrorsPlugin(),
+            new ExtractTextPlugin('stylesheets/[name].css'),
+            new OptimizeCssAssetsPlugin(),
+            new HtmlWebpackPlugin({
+                template: 'index.ejs'
+            })
+        ],
+        module: {
+            loaders: [
+                {
+                    test: /\.(png|svg|jpg|gif|woff|woff2|eot|ttf|otf)$/,
+                    loader: 'url-loader?limit=100000'
+                },
+                {
+                    test: /\.css$/,
+                    loader: ExtractTextPlugin.extract(
+                        {
+                            fallback: 'style-loader',
+                            use: 'css-loader'
+                        }
+                    )
+                }
+            ]
+        }
+    };
+    // Production only
+    if (env.production) {
+        config.plugins.push(new webpack.optimize.UglifyJsPlugin())
     }
+    return config;
 };
